dd8cbdb5d57733bb6470fa954eee120f74ef7d92
maxime <maxime.curon@std.heh.be>
2023-04-04 22:17:20 +0200

    fix display

---
+++ src/lib/components/title/H2.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    export let text: string;
+</script>
+
+<h2>
+    {text}
+</h2>
+
+<style>
+    h2{
+        text-align: center;
+        color: white;
+        font-size: 1.5rem;
+        font-weight: bold;
+    }
+</style>
--- src/routes/index/+page.svelte
+++ src/routes/index/+page.svelte
@@ -1,8 +1,8 @@
 <script lang='ts'>
-    import H1 from '$lib/components/title/H1.svelte';
+    import H2 from '$lib/components/title/H2.svelte';
 	import type { PageData } from './$types';
 
     export let data:PageData;
 </script>
 
-<H1 text="Welcome {data.auth.user?.name}"></H1>
+<H2 text="Welcome {data.auth.user?.name}"></H2>
